6ecb58699234a0ebde804ae1c6d814d58857bba5
Jenny <evgeniia.vader@gmail.com>
2022-02-06 14:52:03 +0200

    header added

---
+++ public/README.md
@@ -0,0 +1,12 @@
+# Fibonacci project
+
+My second project for Front-End Development Program. You can save calculations in the firebase database and sort it by date or input number. It's a simple bootstrap design.
+
+Here you can find:
+
+1. my Fibonacci function with recursion;
+2. sorting function;
+3. work with realtime database;
+4. loader, Date, memo function;
+
+You can try [it](https://fibonacci-project.web.app/)
---
+++ public/app/app.js
@@ -0,0 +1,210 @@
+const btn = document.getElementById('button');
+const input = document.getElementById('input');
+const block = document.getElementById('input-wrapper');
+const spinner = document.querySelector('.spinner-border');
+const spinnerTwo = document.querySelector('.second');
+const results = document.querySelector('.results');
+const errorMessage = document.querySelectorAll('.invalid-tooltip');
+const save = document.getElementById('saving');
+const select = document.getElementById('filter');
+spinner.hidden = true;
+spinnerTwo.hidden = false;
+
+fetchRes();
+
+/* button event */
+btn.addEventListener('click', () => {
+  if (spinner.hidden) {
+    spinner.hidden = false;
+  } else {
+    spinner.hidden = true;
+  }
+
+  setTimeout(() => {
+    if (input.value > 50) {
+      errorMsg('fifty');
+    } else if (input.value == 42) {
+      errorMsg('mLife');
+    } else if (input.value == '') {
+      errorMsg('empty');
+    } else if (input.value < 0) {
+      errorMsg('negative');
+    } else {
+      checkbtn(save);
+    }
+    spinner.hidden = true;
+  }, 2000);
+
+  refresh();
+  errorReload(errorMessage);
+});
+
+/* invalid feedback */
+function errorMsg(id) {
+  const msg = document.getElementById(id);
+  msg.style.display = 'block';
+
+  input.style.color = '#D9534F';
+  input.style.borderColor = '#D9534F';
+}
+
+async function fetchNum() {
+  try {
+    const url = `http://localhost:5050/fibonacci/${input.value}`;
+    const res = await axios.get(url);
+    addRes(res.data.result);
+    fetchRes();
+  } catch (error) {
+    console.log(error);
+    alert(`${error.message}. Please look in a console`);
+  }
+}
+
+/* refresh page */
+const refresh = () => {
+  const par = document.getElementById('answer');
+  //   if there's no par, do nothing
+  if (!par);
+  else par.remove();
+};
+
+/* remove error msg */
+const errorReload = (x) => {
+  input.style.color = '#000';
+  input.style.borderColor = '#ced4da';
+
+  if (!x);
+  else {
+    x.forEach((msg) => {
+      if (msg.style.display == 'block') {
+        msg.style.display = 'none';
+      }
+    });
+  }
+};
+
+/* refresh result block */
+const resultReload = () => {
+  document.querySelectorAll('.line').forEach((e) => e.remove());
+};
+
+/* make result paragraph */
+const addRes = (f) => {
+  const resP = document.createElement('p');
+  resP.id = 'answer';
+  resP.innerText = f;
+  block.append(resP);
+};
+
+async function fetchRes() {
+  try {
+    const url = `http://localhost:5050/getFibonacciResults`;
+    const res = await axios.get(url);
+    resBlock(res.data);
+    spinnerTwo.hidden = true;
+  } catch (error) {
+    console.log(error);
+    alert(`${error.message}. Please look in a console`);
+  }
+}
+
+/* prev. results block*/
+function resBlock(data) {
+  const prevRes = data.results;
+  console.log(data.results)
+  let arr = prevRes.sort(function (a, b) {
+    return b.createdDate - a.createdDate;
+  });
+
+  for (let i = 0; i < 10; i++) {
+    makeLineRes(arr[i], prevRes[i]);
+  }
+}
+
+/* make result divs for result block */
+function makeLineRes(datearr, res) {
+  const line = document.createElement('div');
+  let date = datearr.createdDate;
+  let newDate = new Date(date);
+  line.innerText = `The Fibonnaci Of ${res.number} is ${res.result}. Calculated at: ${newDate}`;
+  line.className = 'line';
+  line.setAttribute('data-filter', res.number);
+  line.setAttribute('data-date', date);
+  results.append(line);
+}
+
+/* select filter*/
+select.addEventListener('click', () => {
+  let lines = document.querySelectorAll('.line');
+  switch (select.value) {
+    case 'nASC':
+      resultReload();
+      Array.from(lines)
+        .sort((a, b) => {
+          return a.dataset.filter - b.dataset.filter;
+        })
+        .forEach((el) => {
+          results.append(el);
+        });
+
+      break;
+    case 'nDSC':
+      resultReload();
+      Array.from(lines)
+        .sort((a, b) => {
+          return b.dataset.filter - a.dataset.filter;
+        })
+        .forEach((el) => {
+          results.append(el);
+        });
+      break;
+    case 'dASC':
+      resultReload();
+      Array.from(lines)
+        .sort((a, b) => {
+          return a.dataset.date - b.dataset.date;
+        })
+        .forEach((el) => {
+          results.append(el);
+        });
+      break;
+    case 'dDSC':
+      resultReload();
+      Array.from(lines)
+        .sort((a, b) => {
+          return b.dataset.date - a.dataset.date;
+        })
+        .forEach((el) => {
+          results.append(el);
+        });
+      break;
+  }
+});
+
+/* save button check */
+function checkbtn(btn) {
+  if (btn.checked) {
+    resultReload();
+    fetchNum();
+    spinnerTwo.hidden = false;
+  } else {
+    const val = input.value;
+    let res = fibN(val);
+    addRes(res);
+  }
+}
+
+/* my Fibonacci function */
+const fib = (n) => {
+  if (n <= 1) {
+    return n;
+  } else {
+    return fib(n - 1) + fib(n - 2);
+  }
+};
+
+const memo = (x) => {
+  const cache = {};
+  return (arg) => cache[arg] || (cache[arg] = x(arg));
+};
+const fibN = memo(fib);
